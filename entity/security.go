ecb1350437155241eefebfd0d94b9c629a8d5afd
stephenlyu <64734301@qq.com>
2018-05-01 11:29:31 +0800

    A minor refactoring.

--- entity/security.go
+++ entity/security.go
@@ -3,13 +3,14 @@ package entity
 import (
 	"regexp"
 	"errors"
-	"fmt"
 	"strings"
 )
 
 type Security struct {
 	Code string
 	Exchange string
+
+	fullCode string
 }
 
 var codePattern, _ = regexp.Compile(`^([0-9a-zA-Z]+)\.([0-9a-zA-Z]+)$`)
@@ -22,7 +23,8 @@ func ParseSecurity(securityCode string) (*Security, error) {
 
 	code := string(matches[1])
 	exchange := strings.ToUpper(string(matches[2]))
-	return &Security{code, exchange}, nil
+
+	return &Security{code, exchange, securityCode}, nil
 }
 
 func ParseSecurityUnsafe(securityCode string) (*Security) {
@@ -31,7 +33,7 @@ func ParseSecurityUnsafe(securityCode string) (*Security) {
 }
 
 func (this *Security) String() string {
-	return fmt.Sprintf("%s.%s", this.Code, this.Exchange)
+	return this.fullCode
 }
 
 func (this *Security) GetCode() string {
