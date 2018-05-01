669721a4a4c1a4114520f795bbffe7eca199c62c
stephenlyu <64734301@qq.com>
2018-05-01 11:03:55 +0800

    Add PanicTrace method.

--- util/util.go
+++ util/util.go
@@ -7,6 +7,8 @@ import (
 	"io"
 	"path/filepath"
 	"math"
+	"runtime"
+	"bytes"
 )
 
 func Assert(cond bool, msg string) {
@@ -81,4 +83,26 @@ func Round(val float64, places int) float64 {
 	}
 
 	return t
+}
+
+func PanicTrace(kb int) []byte {
+	s := []byte("/src/runtime/panic.go")
+	e := []byte("\ngoroutine ")
+	line := []byte("\n")
+	stack := make([]byte, kb<<10) //4KB
+	length := runtime.Stack(stack, true)
+	start := bytes.Index(stack, s)
+	stack = stack[start:length]
+	start = bytes.Index(stack, line) + 1
+	stack = stack[start:]
+	end := bytes.LastIndex(stack, line)
+	if end != -1 {
+		stack = stack[:end]
+	}
+	end = bytes.Index(stack, e)
+	if end != -1 {
+		stack = stack[:end]
+	}
+	stack = bytes.TrimRight(stack, "\n")
+	return stack
 }
