3d240f9d4a0554791be6557759f642b2899838e8
stephenlyu <64734301@qq.com>
2018-08-18 18:18:22 +0800

    Refactoring entity.Security to support more securities.

--- entity/security.go
+++ entity/security.go
@@ -7,24 +7,48 @@ import (
 )
 
 type Security struct {
-	Code string
+	Category string
+	Code     string
 	Exchange string
 
 	fullCode string
 }
 
-var codePattern, _ = regexp.Compile(`^([0-9a-zA-Z]+)\.([0-9a-zA-Z]+)$`)
+var (
+	aShareCodePattern, _ = regexp.Compile(`^()([0-9]+)\.([0-9a-zA-Z]+)$`)
+	okexFutureCodePattern, _ = regexp.Compile(`^([A-Z]+)([TNQ]FUT)\.(OKEX)$`)
+	dcSpotCodePattern, _  = regexp.Compile(`^([A-Z]+)(SPOT)\.([A-Z]+)$`)
+	cnCommodityFutureCodePattern, _ = regexp.Compile(`^([A-Z]+)([0-9]+)\.([A-Z]+)$`)
+	codePatterns = []*regexp.Regexp{aShareCodePattern, okexFutureCodePattern, dcSpotCodePattern, cnCommodityFutureCodePattern}
+)
 
 func ParseSecurity(securityCode string) (*Security, error) {
-	matches := codePattern.FindSubmatch([]byte(securityCode))
+	securityCode = strings.ToUpper(securityCode)
+
+	var matches [][]byte
+	for _, p := range codePatterns {
+		matches = p.FindSubmatch([]byte(securityCode))
+		if len(matches) > 0 {
+			break
+		}
+	}
+
 	if len(matches) == 0 {
 		return nil, errors.New("Bad security code")
 	}
 
-	code := string(matches[1])
-	exchange := strings.ToUpper(string(matches[2]))
+	var cat, code, exchange string
+	switch len(matches) {
+	case 3:
+		code = string(matches[1])
+		exchange = string(matches[2])
+	case 4:
+		cat = string(matches[1])
+		code = string(matches[2])
+		exchange = string(matches[3])
+	}
 
-	return &Security{code, exchange, securityCode}, nil
+	return &Security{cat, code, exchange, securityCode}, nil
 }
 
 func ParseSecurityUnsafe(securityCode string) (*Security) {
@@ -36,10 +60,21 @@ func (this *Security) String() string {
 	return this.fullCode
 }
 
+func (this *Security) GetCategory() string {
+	if this.Category == "" {
+		return "ASTOCK"
+	}
+	return this.Category
+}
+
 func (this *Security) GetCode() string {
 	return this.Code
 }
 
 func (this *Security) GetExchange() string {
 	return this.Exchange
+}
+
+func (this *Security) IsSpot() bool {
+	return this.Code == "SPOT"
 }
