c869bc6b5d586de49b3da4fd6d19aaa236fedae4
stephenlyu <64734301@qq.com>
2018-08-21 17:40:28 +0800

    Add csvdatasource implementation.

---
+++ datasource/csv/csvdatasource.go
@@ -0,0 +1,126 @@
+package csvdatasource
+
+import (
+	. "github.com/stephenlyu/tds/entity"
+	. "github.com/stephenlyu/tds/period"
+	"github.com/stephenlyu/tds/datasource"
+	"github.com/stephenlyu/tds/storage"
+	"fmt"
+	"path/filepath"
+	"reflect"
+	"os"
+)
+
+type _CSVDataSource struct {
+	engine *storage.CsvEngine
+	csvDir string
+}
+
+func NewCSVDataSource(csvDir string) datasource.BaseDataSource {
+	return &_CSVDataSource{
+		csvDir: csvDir,
+		engine: storage.NewCsvEngine(reflect.TypeOf(Record{})),
+	}
+}
+
+func (this *_CSVDataSource) filePath(security *Security, period Period) string {
+	fileName := fmt.Sprintf("%s.%s.csv", period.ShortName(), security.String())
+	return filepath.Join(this.csvDir, fileName)
+}
+
+func (this *_CSVDataSource) GetData(security *Security, period Period) (error, []Record) {
+	filePath := this.filePath(security, period)
+
+	err, data := this.engine.Load(filePath)
+	if err != nil {
+		return err, nil
+	}
+
+	ret := make([]Record, len(data))
+	for i := range data {
+		r := data[i].(*Record)
+		ret[i] = *r
+	}
+	return nil, ret
+}
+
+func (this *_CSVDataSource) GetDataEx(security *Security, period Period, startDate uint64, count int) (error, []Record) {
+	err, records := this.GetData(security, period)
+	if err != nil {
+		return err, nil
+	}
+
+	for i := range records {
+		if records[i].Date >= startDate {
+			end := i + count
+			if end > len(records) {
+				end = len(records)
+			}
+			ret := make([]Record, end - i)
+			copy(ret, records[i:end])
+			return nil, ret
+		}
+	}
+	return nil, nil
+}
+
+func (this *_CSVDataSource) GetRangeData(security *Security, period Period, startDate, endDate uint64) (error, []Record) {
+	err, records := this.GetData(security, period)
+	if err != nil {
+		return err, nil
+	}
+
+	var start, end int
+	for i := range records {
+		if records[i].Date >= startDate {
+			start = i
+			break
+		}
+	}
+	end = len(records)
+	for i := start + 1; i < len(records); i++ {
+		if records[i].Date > endDate {
+			end = i
+			break
+		}
+	}
+
+	if end > start {
+		ret := make([]Record, end - start)
+		copy(ret, records[start:end])
+		return nil, ret
+	}
+
+	return nil, nil
+}
+
+func (this *_CSVDataSource) GetDataFromLast(security *Security, period Period, endDate uint64, count int) (error, []Record) {
+	panic("unimplented")
+	return nil, nil
+}
+
+func (this *_CSVDataSource) GetLastRecord(security *Security, period Period) (error, *Record) {
+	panic("unimplented")
+	return nil, nil
+}
+
+func (this *_CSVDataSource) AppendData(security *Security, period Period, data []Record) error {
+	panic("unimplented")
+	return nil
+}
+
+func (this *_CSVDataSource) SaveData(security *Security, period Period, data []Record) error {
+	os.MkdirAll(this.csvDir, 0777)
+	filePath := this.filePath(security, period)
+	dataCpy := make([]interface{}, len(data))
+	for i := range data {
+		dataCpy[i] = &data[i]
+	}
+
+	return this.engine.Save(filePath, dataCpy)
+}
+
+func (this *_CSVDataSource) RemoveData(security *Security, period Period, startDate, endDate uint64) error {
+	panic("unimplented")
+	return nil
+}
---
+++ datasource/csv/csvdatasource_test.go
@@ -0,0 +1,47 @@
+package csvdatasource_test
+
+import (
+	"testing"
+	"github.com/stephenlyu/tds/datasource/tdx"
+	"github.com/stephenlyu/tds/period"
+	"github.com/stephenlyu/tds/util"
+	"github.com/stephenlyu/tds/entity"
+	"fmt"
+	"github.com/stephenlyu/tds/datasource/csv"
+)
+
+func Test_CSVDataSource_SaveData(t *testing.T) {
+	security, err := entity.ParseSecurity("000001.SZ")
+	util.Assert(err == nil, "")
+	util.Assert(security != nil, "")
+
+	err, period1 := period.PeriodFromString("M1")
+	util.Assert(err == nil, "")
+
+	tdxDs := tdxdatasource.NewDataSource("../tdx/data", true)
+
+	err, data := tdxDs.GetData(security, period1)
+	util.Assert(err == nil, "")
+
+	csvDs := csvdatasource.NewCSVDataSource("csv")
+	err = csvDs.SaveData(security, period1, data[:1000])
+	util.Assert(err == nil, fmt.Sprintf("%+v", err))
+}
+
+func Test_CSVDataSource_GetData(t *testing.T) {
+	csvDs := csvdatasource.NewCSVDataSource("csv")
+	security, err := entity.ParseSecurity("000001.SZ")
+	util.Assert(err == nil, "")
+	util.Assert(security != nil, "")
+
+	err, period1 := period.PeriodFromString("M1")
+	util.Assert(err == nil, "")
+
+	start := util.Tick()
+	err, data := csvDs.GetData(security, period1)
+	fmt.Printf("time cost: %dms\n", util.Tick() - start)
+	util.Assert(err == nil, "")
+	fmt.Println(len(data))
+	fmt.Printf("%+v\n", &data[0])
+	fmt.Printf("%+v\n", &data[len(data) - 1])
+}
