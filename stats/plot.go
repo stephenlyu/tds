e7750ac554b11d099677079c5435b7ddea497aae
stephenlyu <64734301@qq.com>
2018-12-01 12:29:27 +0800

    Add log scale support for plot API

--- stats/plot.go
+++ stats/plot.go
@@ -8,6 +8,7 @@ import (
 	"github.com/stephenlyu/tds/util"
 	"image/color"
 	"github.com/gonum/plot/vg/draw"
+	"math"
 )
 
 var plotColors = []color.Color {
@@ -26,14 +27,17 @@ var plotColors = []color.Color {
 	color.RGBA{0, 0, 0, 255},
 }
 
-func Plot(title string, titles []string, xTicks []string, plotData map[string][]float64, pdfFile string) error {
+func Plot(title string, titles []string, xTicks []string, plotData map[string][]float64, pdfFile string, useLogScale bool) error {
 	util.Assert(len(titles) == len(plotData), "")
 	convert := func (values []float64) plotter.XYs {
 		pts := make(plotter.XYs, len(values))
 		for i := range pts {
 			pts[i].X = float64(i)
-			pts[i].Y = values[i]
-
+			if useLogScale {
+				pts[i].Y = math.Log(values[i])
+			} else {
+				pts[i].Y = values[i]
+			}
 		}
 		return pts
 	}
@@ -46,7 +50,7 @@ func Plot(title string, titles []string, xTicks []string, plotData map[string][]
 	p.Title.Text = title
 	p.X.Label.Text = ""
 	p.Y.Label.Text = ""
-	p.Y.Tick.Marker = round2Ticks{}
+	p.Y.Tick.Marker = round2Ticks{useLogScale: useLogScale}
 
 	if xTicks != nil {
 		p.X.Tick.Marker = stringTicks{ticks: xTicks}
@@ -76,16 +80,24 @@ func Plot(title string, titles []string, xTicks []string, plotData map[string][]
 	return p.Save(297*vg.Millimeter, 210*vg.Millimeter, pdfFile)
 }
 
-type round2Ticks struct{}
+type round2Ticks struct{
+	useLogScale bool
+}
 // Ticks computes the default tick marks, but inserts commas
 // into the labels for the major tick marks.
-func (round2Ticks) Ticks(min, max float64) []plot.Tick {
+func (this round2Ticks) Ticks(min, max float64) []plot.Tick {
 	tks := plot.DefaultTicks{}.Ticks(min, max)
 	for i, t := range tks {
 		if t.Label == "" { // Skip minor ticks, they are fine.
 			continue
 		}
-		tks[i].Label = fmt.Sprintf("%.02f", t.Value)
+		var value float64
+		if this.useLogScale {
+			value =  math.Exp(t.Value)
+		} else {
+			value = t.Value
+		}
+		tks[i].Label = fmt.Sprintf("%.02f", value)
 	}
 	return tks
 }
